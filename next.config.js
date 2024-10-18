dad46a6f80083f197e85c4b63cdd30cc5a9085f3
Ahet34 <alantintaya34@gmail.com>
2024-10-18 18:42:45 -0400

    imagenes urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   images: {
-    domains: ["artecampo.com.bo", 'scontent.flpb4-1.fna.fbcdn.net'], 
+    domains: ['/images',"artecampo.com.bo", 'scontent.flpb4-1.fna.fbcdn.net', 't1.uc.ltmcdn.com', 'jaimaalkauzar.es', 'i0.wp.com', 'www.boliviamall.com', 'us.cdn.eltribuno.com', 'i.etsystatic.com', 'i5.walmartimages.com','ethnoworldliving.com', 'dbhg6mekyuoi1.cloudfront.net', 'galeriaseltriunfo.com', 'artesaniadecastillalamancha.es', 'www.artesanum.com', 'f.fcdn.app', 'scontent.flpb4-1.fna.fbcdn.net', 'pbs.twimg.com' ,'media.sketchfab.com', 'm.media-amazon.com'] 
   },
 };
 

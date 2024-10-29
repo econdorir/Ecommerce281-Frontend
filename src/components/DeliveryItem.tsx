1f9c26d96042e72e4e500d1556a3b6c723ef22e6
Ahet34 <alantintaya34@gmail.com>
2024-10-29 01:17:47 -0400

    alffin xd

--- src/components/DeliveryItem.tsx
+++ src/components/DeliveryItem.tsx
@@ -3,15 +3,22 @@ import Link from 'next/link';
 const DeliveryItem = ({ delivery, deliveryNumber }) => {
   return (
     <div className="border-b py-2 flex justify-between">
-      <div>
-        <p>Entrega #{deliveryNumber}</p>
-        <p>Estado: {delivery.estado}</p>
-        <p>Fecha: {delivery.fecha_entrega}</p>
-      </div>
-      <Link href={`/deliveriesDelivery/${delivery.id}`}>
-        <button className="bg-green-500 text-white px-3 py-1 rounded">Ver Entrega</button>
-      </Link>
-    </div>
+  <div>
+    <p>Entrega #{deliveryNumber}</p>
+    <p>Estado: {delivery.estado}</p>
+    <p>Fecha: {delivery.fecha_entrega}</p>
+  </div>
+  
+  <div className="flex flex-col space-y-2 items-center">
+    <Link href={`/deliveries/${delivery.id}`}>
+      <button className="bg-orange-500 text-white px-3 py-1 rounded">Confirmaciones</button>
+    </Link>
+    <Link href={`/deliveriesDelivery/${delivery.id}`}>
+      <button className="bg-green-500 text-white px-3 py-1 rounded">Ver Detalles</button>
+    </Link>
+  </div>
+</div>
+
   );
 };
 

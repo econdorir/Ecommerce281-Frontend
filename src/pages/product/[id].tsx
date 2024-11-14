0e3f19017619df42547448ad7f119dc59ff33244
eleazar <eledavid88@gmail.com>
2024-11-14 00:27:25 -0400

    mobileslideshow

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -175,7 +175,7 @@ const ProductDetail = ({ product, resenia, clientes, promociones }) => {
           <ProductMobileSlideShow
             title={product.nombre_producto}
             images={
-              product.imagen[0]?.url_imagen ||
+              product.imagen||
               "https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg"
             }
             className="block md:hidden"

13d683cfad0ac2ba2eead0388ee5068b63ac2547
ChaconBit <chaconabrahamj@gmail.com>
2024-12-05 19:53:09 -0400

    actualice productForm

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { CldUploadWidget } from "next-cloudinary";
 import { CreateProductService } from "@/services/CreateProductService";
@@ -15,17 +17,21 @@ interface ProductFormData {
   envio_gratuito: number;
   stock_producto: number;
   images: {
-    image1?: string;
-    image2?: string;
-    image3?: string;
+    image1?: string; // Opcional
+    image2?: string; // Opcional
+    image3?: string; // Opcional
   };
 }
 
 export default function ProductForm() {
   const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [imageFiles, setImageFiles] = useState<File[]>([]); // Guardar archivos de imagen
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [errorMaxImage, setErrorMaxImage] = useState<string | null>(null);
   const [freeShipping, setFreeShipping] = useState(false);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [uploadedImageUrls, setUploadedImageUrls] = useState<string[]>([]);
 
   const [formData, setFormData] = useState<ProductFormData>({
     id_artesano: 1,
@@ -50,27 +56,56 @@ export default function ProductForm() {
     });
   };
 
-  const handleUploadSuccess = (result: any) => {
-    // Obtener las URLs de las imágenes subidas
-    const newImageUrls = result?.info?.url ? [...imageUrls, result.info.url] : imageUrls;
-
-    if (newImageUrls.length <= 3) {
-      setImageUrls(newImageUrls);
-    } else {
-      alert("No puedes subir más de 3 imágenes.");
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(event.target.files as FileList);
+    if (files.length + selectedFiles.length > 3) {
+      setErrorMaxImage("Puedes subir un máximo de 3 imágenes.");
+      return;
     }
+    setErrorMaxImage(null);
+    setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
   };
 
+  const handleRemoveFile = (index, event) => {
+    event.preventDefault();
+    setSelectedFiles((prevFiles) =>
+      prevFiles.filter((_, fileIndex) => fileIndex !== index)
+    );
+  };
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    console.log("primero:", JSON.stringify(formData));
+    
     try {
+      const urls: string[] = []; // Arreglo temporal para almacenar URLs
+
+      // Subir imágenes y obtener URLs
+      for (const file of selectedFiles) {
+        const imageFormData = new FormData();
+        imageFormData.append("image", file);
+
+        const response = await fetch("/api/upload", {
+          method: "POST",
+          body: imageFormData,
+        });
+
+        if (!response.ok) {
+          const errorMessage = await response.text();
+          throw new Error(`Error: ${response.status} ${errorMessage}`);
+        }
+
+        const data = await response.json();
+        urls.push(data.url);
+      }
+
+      // Actualizar formData con las nuevas imágenes
       const updatedFormData = {
         ...formData,
         images: {
-          image1: imageUrls[0] || undefined,
-          image2: imageUrls[1] || undefined,
-          image3: imageUrls[2] || undefined,
+          image1: urls[0] || undefined,
+          image2: urls[1] || undefined,
+          image3: urls[2] || undefined,
         },
         envio_gratuito: freeShipping ? 1 : 0,
       };
@@ -106,7 +141,7 @@ export default function ProductForm() {
       envio_gratuito: 0,
       images: {},
     });
-    setImageUrls([]);
+    setSelectedFiles([]);
   };
 
   return (
@@ -200,35 +235,70 @@ export default function ProductForm() {
         />
       </div>
 
-      {/* Imágenes */}
+      {/* Dimensiones */}
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        <div>
+          <label className="block text-white font-medium">Largo (cm)</label>
+          <input
+            type="number"
+            name="largo_producto"
+            value={formData.largo_producto}
+            onChange={handleChange}
+            className="w-full bg-extrapagecolor2 text-buttonpagecolor2 mt-1 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+            min={1}
+          />
+        </div>
+        <div>
+          <label className="block text-white font-medium">Ancho (cm)</label>
+          <input
+            type="number"
+            name="ancho_producto"
+            value={formData.ancho_producto}
+            onChange={handleChange}
+            className="w-full bg-extrapagecolor2 text-buttonpagecolor2 mt-1 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+            min={1}
+          />
+        </div>
+        <div>
+          <label className="block text-white font-medium">Alto (cm)</label>
+          <input
+            type="number"
+            name="alto_producto"
+            value={formData.alto_producto}
+            onChange={handleChange}
+            className="w-full bg-extrapagecolor2 text-buttonpagecolor2 mt-1 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+            min={1}
+          />
+        </div>
+      </div>
+
       <div className="mb-4">
         <label className="block text-white">Imágenes</label>
-        <CldUploadWidget
-          onUpload={handleUploadSuccess}
-          uploadPreset="your_cloudinary_upload_preset"
-          options={{ maxFiles: 3 }}
-        >
-          {({ open }) => (
-            <button
-              type="button"// Aquí no es necesario envolver open con una función anónima.
-              className="w-full bg-extrapagecolor2 text-buttonpagecolor2 mt-1 p-2 border border-gray-300 rounded-md"
-            >
-              Subir Imágenes
-            </button>
-          )}
-        </CldUploadWidget>
-
-        {imageUrls.length > 0 && (
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleFileChange}
+          className="w-full bg-extrapagecolor2 text-buttonpagecolor2 mt-1 p-2 border border-gray-300 rounded-md"
+        />
+        {errorMaxImage && <p className="text-red-500 mt-2">{errorMaxImage}</p>}{" "}
+        {selectedFiles.length > 0 && (
           <div className="mt-4">
-            <h3 className="text-white">Imágenes seleccionadas:</h3>
+            <h3 className="text-white">Selected Images:</h3>
             <div className="flex flex-wrap mt-2">
-              {imageUrls.map((url, index) => (
+              {selectedFiles.map((file, index) => (
                 <div key={index} className="relative w-24 h-24 m-2">
                   <img
-                    src={url}
-                    alt={`Imagen ${index + 1}`}
+                    src={URL.createObjectURL(file)}
+                    alt={file.name}
                     className="w-full h-full object-cover rounded-md"
                   />
+                  <button
+                    onClick={(event) => handleRemoveFile(index, event)}
+                    className="w-9 absolute -top-3 -right-3 bg-red-500 text-white rounded-full p-1 shadow-md hover:bg-red-700 "
+                  >
+                    X
+                  </button>
                 </div>
               ))}
             </div>
@@ -236,36 +306,82 @@ export default function ProductForm() {
         )}
       </div>
 
-      {/* Envío gratuito */}
-      <div className="flex items-center">
+      {/* <CldUploadWidget
+        options={{ sources: ["local", "url", "camera"] }}
+        uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME}
+        onSuccess={handleUpload}
+      >
+        {({ open }) => {
+          return (
+            <button
+              className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200"
+              onClick={() => open()}
+            >
+              Elegir Archivos
+            </button>
+          );
+        }}
+      </CldUploadWidget> */}
+
+      {/* Etiquetas */}
+      {/* <div className="mb-4">
+        <label className="block text-white">Etiquetas (separadas por comas)</label>
         <input
-          type="checkbox"
-          name="envio_gratuito"
-          checked={freeShipping}
-          onChange={() => setFreeShipping(!freeShipping)}
-          className="mr-2"
+          type="text"
+          name="tags"
+          value={formData.tags}
+          onChange={handleChange}
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md"
         />
-        <label className="text-white">¿Envio gratuito?</label>
+      </div> */}
+
+      {/* Envío gratuito */}
+      <div className="mb-4">
+        <label className="inline-flex items-center">
+          <input
+            type="checkbox"
+            name="freeShipping"
+            onChange={handleChange}
+            className="mr-2"
+          />
+          Envío gratuito
+        </label>
       </div>
 
-      {/* Mensajes de éxito o error */}
-      {successMessage && (
-        <div className="mt-4 p-4 bg-green-500 text-white rounded-md">
-          {successMessage}
+      {/* A partir de qué cantidad se habilita el envío gratuito */}
+      {/* {formData.freeShipping && (
+        <div className="mb-4">
+          <label className="block text-white">A partir de qué cantidad</label>
+          <input
+            type="number"
+            name="shippingThreshold"
+            value={formData.shippingThreshold}
+            onChange={handleChange}
+            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+          />
         </div>
-      )}
-      {errorMessage && (
-        <div className="mt-4 p-4 bg-red-500 text-white rounded-md">
-          {errorMessage}
-        </div>
-      )}
+      )} */}
 
-      {/* Botón para enviar */}
+      {/* Ubicación */}
+      {/* <div className="mb-4">
+        <label className="block text-white">Ubicación</label>
+        <input
+          type="text"
+          name="location"
+          value={formData.location}
+          onChange={handleChange}
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+        />
+      </div> */}
+      {successMessage && (
+        <div className="text-green-600 mb-4">{successMessage}</div>
+      )}
+      {errorMessage && <div className="text-red-600 mb-4">{errorMessage}</div>}
       <button
         type="submit"
-        className="w-full bg-buttonpagecolor2 text-white py-3 rounded-md mt-6 hover:bg-buttonpagecolor1 transition"
+        className="w-full bg-extrapagecolor text-black p-3 rounded-lg hover:bg-blue-700 transition duration-200"
       >
-        Subir Producto
+        Añadir Producto
       </button>
     </form>
   );

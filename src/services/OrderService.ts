fa070563b93b6759393db51d2a21d1cdc8d89e0a
ChaconBit <chaconabrahamj@gmail.com>
2024-12-09 15:35:04 -0400

    OrderService.ts

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,24 +1,4 @@
 import { API_URL } from "../libs/constants";
-import io from "socket.io-client";
-
-// Conexión con el servidor WebSocket en el puerto 4000
-const socket = io("http://localhost:4000"); // Cambia la URL al puerto correcto
-
-// Verificar la conexión al servidor WebSocket
-socket.on("connect", () => {
-  console.log("Conectado al servidor WebSocket");
-});
-
-socket.on("connect_error", (err) => {
-  console.error("Error de conexión:", err);
-});
-
-// Escuchar el evento 'notify_deliveries' para recibir notificaciones
-socket.on("notify_deliveries", (data) => {
-  console.log("Nueva notificación para deliveries:", data.message);
-  console.log("Detalles del pedido:", data.orderDetails);
-  // Aquí puedes agregar lógica para mostrar notificaciones al usuario
-});
 
 export const createOrder = async (orderData) => {
   try {
@@ -52,11 +32,6 @@ export const createOrder = async (orderData) => {
       localStorage.setItem("userData", JSON.stringify(newUserData));
     }
 
-    // Emitir el evento de "new_order" para notificar a los deliveries
-    socket.emit("new_order", {
-      message: `Nuevo pedido de ${userData.name || "un cliente desconocido"}`,
-      orderDetails: data.nuevoCliente, // Puedes personalizar los detalles que envías
-    });
 
     return data; // Devuelve la respuesta si es necesario
   } catch (error) {
